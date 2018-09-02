9a0eb04bf65356e21545b076afbe6babf10ff577
lal <eduardo.rasgado@hotmail.com>
2018-09-02 17:59:25 -0500

    secuencia de tabuladores

--- FirstProject/main.cpp
+++ FirstProject/main.cpp
@@ -1,7 +1,21 @@
+/*
+archivos de cabecera
+o headers
+*/
 #include <iostream>
 
 int main()
 {
-    std::cout << "Este es mi primer programa del curso de c++" << std::endl;
+    /*
+    SECUENCUAS DE ESCAPE
+    \n -> salto de linea
+    \b -> retroceso
+    \r -> retorno de carro
+    \t -> tabulador
+    */
+    std::cout << "Este es mi primer programa del curso de c++\n" << "Mi nombre es Eduardo Rasgado Ruiz" << std::endl;
+    // este mensaje se va a sobreponer con el segundo
+    std::cout << "Hola este mensaje es impreso\r" << "Hola de nuevo" << std::endl;
+    std::cout << "Hola\t" << "Hola despues del TAB" << std::endl;
     return 0;
 }
