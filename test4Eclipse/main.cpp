9801fe2742ffedb98e51066fa0a6799cf8f674c0
lal <eduardo.rasgado@hotmail.com>
2018-09-02 20:22:09 -0500

    eclipse testing

---
+++ test4Eclipse/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Sep 2, 2018
+ *      Author: Eduardo Rasgado Ruiz
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Hola Eclipse para C++ x2" << endl;
+	return 0;
+}
+
---
+++ thirthProject/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Sep 2, 2018
+ *      Author: cheetos
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Hola Eclipse para C++" << endl;
+	return 0;
+}
+
