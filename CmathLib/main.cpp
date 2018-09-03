24b9c83d91c721abfa6e072cfc50504a8106395a
lal <eduardo.rasgado@hotmail.com>
2018-09-03 15:52:49 -0500

    trabajndo con cmath library

--- CmathLib/main.cpp
+++ CmathLib/main.cpp
@@ -10,13 +10,19 @@ int main()
     /**CMATH LIBRARY**/
     double potencia = pow(5,2);
     double raiz = sqrt(81.0);
-    double angle = 0.523599;
+
+    double angle = 0.523599; // seno de 30 grados
     double seno = sin(angle);
+
+    double coseno = cos(1.0472); // coseno de 60 grados
+    double tangente = tan(0.785398); // tangente de 45 grados
 
     // printing everything
     cout << potencia << endl;
     cout << raiz << endl;
     cout << seno << endl;
+    cout << coseno << endl;
+    cout << tangente << endl;
 
     return 0;
 }
