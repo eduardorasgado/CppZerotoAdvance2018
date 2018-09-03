5b085920f3877abfed0272a616e92dbc81703179
lal <eduardo.rasgado@hotmail.com>
2018-09-03 15:48:22 -0500

    cmath library

---
+++ CmathLib/main.cpp
@@ -0,0 +1,22 @@
+/*
+Author: Eduardo Rasgado Ruiz
+*/
+#include <iostream>
+#include <cmath> // libreria math.h
+using namespace std;
+
+int main()
+{
+    /**CMATH LIBRARY**/
+    double potencia = pow(5,2);
+    double raiz = sqrt(81.0);
+    double angle = 0.523599;
+    double seno = sin(angle);
+
+    // printing everything
+    cout << potencia << endl;
+    cout << raiz << endl;
+    cout << seno << endl;
+
+    return 0;
+}
