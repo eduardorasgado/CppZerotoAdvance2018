379a29a1e762e8142c50a808e5b714ea1cf5146b
lal <eduardo.rasgado@hotmail.com>
2018-09-02 23:29:40 -0500

    char y enteros

--- Variables/main.cpp
+++ Variables/main.cpp
@@ -4,14 +4,26 @@ using namespace std;
 
 int main()
 {
-    /*
-    TIPO CHAR
-    PESO EN BYTES = 1
-    N° DE VALORES  = 256
-    rANGO = DE -128 A 127
-    */
-    char letra = 65;
-    cout << letra << endl;
 
+    /**TIPO CHAR**/
+    /*
+    peso en bytes = 1
+    N° de valores  = 256
+    rango = de -128 A 127
+    */
+
+    // codigo ascii
+    char letra = 65;
+    cout << letra << " tiene " << sizeof(char) << " bytes" << endl;
+
+    /**TIPO INT**/
+    /*
+    peso en byte = 4(puede variar)
+    N° de valores = 2^32
+    Rango = de -2,147,483,648 a 2,147,483,648
+    */
+    int numero = 0;
+    // la cantidad de espacio en la memoria
+    cout << numero << " tiene " << sizeof(int) << " bytes"  << endl;
     return 0;
 }
