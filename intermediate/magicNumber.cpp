cf345743ad618c092434dc1adf99b4ad6be2136c
lal <eduardo.rasgado@hotmail.com>
2018-09-03 17:53:09 -0500

    magic number p1

---
+++ intermediate/magicNumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib> // rand()
+#include <ctime> //para generar el numero aleatorio
+
+using namespace std;
+
+int main(){
+    int userNumber, randomNumber, minimum = 1, maximum = 100, contador = 0;
+
+
+    cout << randomNum << endl;
+
+    return 0;
+}
