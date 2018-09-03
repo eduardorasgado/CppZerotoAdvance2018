a1aaf57b453e81540970a5178504963a0e5898a3
lal <eduardo.rasgado@hotmail.com>
2018-09-02 23:22:31 -0500

    variables project

--- SecondProject/main.cpp
+++ SecondProject/main.cpp
@@ -15,12 +15,16 @@ int main()
     int edad =  25, varx = 30;
     char var2 = 'A';
     float var3 = 3.14, promedioFinal = 15.9;
-    double varDouble;
+    double varDouble = 5.3e6;
+    // double usando notacion cientifica
+    double smallValue = 1.222e-6;
     bool varBool= true;
     edad = 24;
 
     cout << "Tu edad es de: " << edad << " años" << endl;
 
-    cout << var2 << "\n" << varBool << "\n" << promedioFinal << endl;
+    cout << var2 << "\n" << varBool << "\n" << promedioFinal << "\n" << endl;
+    cout << "Valor muy pequeño: " << smallValue << "\n" << "Valor muy grande: " << varDouble << endl;
+
     return 0;
 }
---
+++ Variables/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    TIPO CHAR
+    PESO EN BYTES = 1
+    N° DE VALORES  = 256
+    rANGO = DE -128 A 127
+    */
+    char letra = 65;
+    cout << letra << endl;
+
+    return 0;
+}
