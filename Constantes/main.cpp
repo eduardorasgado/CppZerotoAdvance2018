4b16129a0ac86f1ac7299a4d0a55b63382eb3a0a
lal <eduardo.rasgado@hotmail.com>
2018-09-03 15:58:55 -0500

    constantes p2

--- Constantes/main.cpp
+++ Constantes/main.cpp
@@ -1,10 +1,17 @@
+/*
+Author: Eduardo Rasgado Ruiz
+*/
+// tanto include como define al ser directivas del procesador no llevan punto
+// final
 #include <iostream>
+// definiendo una constante
+#define PI 3.141516
 
 using namespace std;
 
 int main()
 {
     /**CONSTANTES**/
-
+    cout << PI << endl;
     return 0;
 }
