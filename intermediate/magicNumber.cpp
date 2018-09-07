723eb547d3d95414b8dc0e0c332a97a7d54e563e
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-07 16:55:15 -0500

    numero magico juego terminado

--- intermediate/magicNumber.cpp
+++ intermediate/magicNumber.cpp
@@ -13,7 +13,26 @@ int main(){
     // acotando el numero dentro de nuestros requerimientos
     randomNumber = minimum + rand() % (maximum +1 - minimum);
 
-    cout << randomNumber << endl;
+    // cout << randomNumber << endl;
+    cout << "=====NUMERO MAGICO: ADIVINA EL NUMERO=====" << endl;
+    cout << "Se ha generado el numero misterioso, es hora de jugar..." << endl;
 
+    do{
+        cout << "Digite un número: " << endl;
+        cin >> userNumber;
+
+        if(userNumber > randomNumber){
+            cout << "Digite un número menor" << endl;
+        }
+
+        if(userNumber < randomNumber){
+            cout << "Digite un numero mayor" << endl;
+        }
+        contador++;
+
+    } while(userNumber != randomNumber);
+
+    cout << "***Felicidades adivinaste el numero***" << endl;
+    cout << "Número de intentos: " << contador << endl;
     return 0;
 }
