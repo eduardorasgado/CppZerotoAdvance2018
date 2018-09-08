2397fe1dd797ffa507018709863e4de4004d49d0
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-07 19:36:25 -0500

    ahora podemos insertar la longitud del vector

--- intermediate/cinVector.cpp
+++ intermediate/cinVector.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 int main(){
-    // vectores
-    int vectorStandard[5];
+    // vector, numero de elementos del vector
+    int vectorStandard[5], vector_length;
+    cout << "Ingrese el numero de elementos que va a tener su vector: " << endl;
+    cin >> vector_length;
 
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < vector_length; i++){
         // pidiendo el dato
         cout << "Por favor ingresa el numero " << i+1 << " del arreglo de 5 elementos: " << endl;
         // insertando el dato dentro del espacio i en el vector
@@ -16,7 +18,7 @@ int main(){
     cout << "\n\n";
     cout << "Y los datos son: " << endl;
 
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < vector_length; i++){
         // mostrando el contenido del vector en pantalla
         cout << "vector[" << i << "]: " << vectorStandard[i] << endl;
     }
