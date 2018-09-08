7ac7b2259464301d809b2419f6a989bee54bad9d
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-07 21:09:45 -0500

    buscando un numero resultante de la suma del resto en un vector

---
+++ intermediate/searchVector.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // buscar un elemento que resulte de la suma del resto de los elementos
+    // del vector
+    int myVector[] = {1,2,3,6};
+
+    // algoritmo
+    for(int i = 0; i < 4; i++) for(int k = 0; k < 4; k++) cout << i << " and " << k << endl;
+
+    return 0;
+}
