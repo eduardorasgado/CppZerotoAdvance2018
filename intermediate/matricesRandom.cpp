2f98c96ccdf58ab92783577185495cb4591cf7dd
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-14 15:30:24 -0500

    random matrices 2

--- intermediate/matricesRandom.cpp
+++ intermediate/matricesRandom.cpp
@@ -6,11 +6,15 @@ int main(){
 	int filas, columnas;
 	cout << "Numero de filas de tu matriz: " << endl, cin >> filas;
 	cout << "Número de columnas de tu matriz: " << endl, cin >> columnas;
-	int matrizRandom[filas][columnas];
+	int matrizRandom[filas][columnas], matrizRandom2[filas][columnas];
+	srand(time(NULL));
 
 	//metiendole datos aleatorios a la matriz
-	for (int i = 0;i < filas;i++) for (int j = 0;j < columnas;j++) matrizRandom[i][j] = rand() % 100;
-	//imprimiendo la matriz
-	for (int i = 0;i < filas;i++) for (int j = 0;j < columnas;j++) cout << matrizRandom[i][j], (j == (columnas - 1)) ? cout << endl : cout << " ";
+	for (int i = 0;i < filas;i++) for (int j = 0;j < columnas;j++) matrizRandom[i][j] = 1 + rand() % 100;
+	//copiando esos valores a otra matriz
+	for (int i = 0;i < filas;i++) for (int j = 0;j < columnas;j++) matrizRandom2[i][j] = matrizRandom[i][j];
+	//imprimiendo la matriz 2
+	for (int i = 0;i < filas;i++) for (int j = 0;j < columnas;j++) (j == 0) ? cout << "|" : cout << "",
+			cout << matrizRandom2[i][j], (j == (columnas - 1)) ? cout << "|"  << endl : cout << " ";
 	return 0;
 }
