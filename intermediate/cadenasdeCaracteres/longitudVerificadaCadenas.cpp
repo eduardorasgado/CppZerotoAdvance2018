b3faa31e7d139f8664c72c725f4e43d39856ef2d
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-14 22:50:40 -0500

    verifying length of a char array

--- intermediate/cadenasdeCaracteres/longitudVerificadaCadenas.cpp
+++ intermediate/cadenasdeCaracteres/longitudVerificadaCadenas.cpp
@@ -4,9 +4,12 @@
 using namespace std;
 
 int main(){
-	char cadena[] = "Esta es una cadena y es muy larga";
-	// char cadena[] = "cortita";
-	(strlen(cadena) >10) ? cout << cadena : cout << ""; cout << endl;
+	char frase[50];
+	cout << "Escriba una cadena de caracteres: ";
+	// toma la informacionde usuario
+	cin.getline(frase, 50, '\n');
+	// si la cadena supera los 10 caracteres entonces se imprime
+	(strlen(frase) >10) ? cout << frase << endl : cout << strlen(frase) << endl;
 
 	return 0;
 }
