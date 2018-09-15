36a66eb7b803d3f2be32ad7272f2af4910859cb2
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-14 19:55:38 -0500

    calculation of symmetry in matrices

--- intermediate/matricesSimetria.cpp
+++ intermediate/matricesSimetria.cpp
@@ -7,23 +7,32 @@ int main(){
 	// determinar simetria de una matriz
 	// matriz simetrica:
 	// es cuadrada n == m y es igual a su transpuesta
-	int matriz[3][3] = {{8, 1, 3}, {1, 7, 4}, {3, 4, 9}};
-	//int matriz[3][4] = {{8, 1, 3, 4}, {1, 7, 4, 5}, {3, 4, 9, 6}};
 	bool size = false, transposeEqual = false;
-	// algoritmo
-	int filas, columnas;
 
+	int filas, columnas, elementosCalculados = 0, totalElementos;
 	// obteniendo filas y columnas
-	filas = end(matriz) - begin(matriz);
-	columnas = end(matriz[0]) - begin(matriz[0]);
-	// comprobando size
+	cout << "digite el numero de filas: ", cin >> filas;
+	cout << "digite el numero de columnas: ", cin >> columnas;
+	int matriz[filas][columnas];
+
+	// insertando elementos en la matriz
+	for (int i = 0;i < filas;i++) for (int j = 0;j < columnas;j++) cout << "ingrese matriz[" << i << "][" << j << "]: ", cin >> matriz[i][j];
+
+	// algoritmo
+	// obteniendo filas y columnas
 	size = (filas == columnas) ? true : false;
 
+	totalElementos = filas * columnas;
+
 	// calculando transpuesta
-	try { for (int i = 0;i < columnas;i++) for (int j = 0;j < filas;j++) transposeEqual = (matriz[i][j] == matriz[j][i]) ? true : false; }
+	try { for (int i = 0;i < filas;i++) for (int j = 0;j < columnas;j++) elementosCalculados = (matriz[i][j] == matriz[j][i]) ? elementosCalculados + 1 : elementosCalculados; }
 	catch(int error) { cout << "ERROR: Desbordamiento \n"; }
+	transposeEqual = (totalElementos == elementosCalculados) ? true : false;
+
 	// si se cumplen ambas afirmaciones
 	(size && transposeEqual) ? cout << "La matriz es simétrica" << endl : cout << "La matriz no es simetrica" << endl;
-
+	//imprimiendo la matriz
+	cout << "La matriz es: " << endl;
+	for (int i = 0;i < filas;i++) for (int j = 0;j < columnas;j++) cout << matriz[i][j], (j == (columnas - 1)) ? cout << endl : cout << " ";
 	return 0;
 }
