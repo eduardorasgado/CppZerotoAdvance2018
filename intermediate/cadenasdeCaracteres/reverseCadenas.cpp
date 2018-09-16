c58fad71e4789d9be58a980497f2aaf38754f8e0
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-16 04:00:19 -0500

    reverse of a string

---
+++ intermediate/cadenasdeCaracteres/reverseCadenas.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	string cadena = "Eduardo";
+	reverse(cadena.begin(), cadena.end());
+	cout << cadena << endl;
+	return 0;
+}
+
+
