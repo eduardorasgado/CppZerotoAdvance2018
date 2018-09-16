085d19f6f66d9ef582391b50ca6161c0c2d6017a
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-16 04:19:11 -0500

    palindrome word algorithm 2

--- intermediate/cadenasdeCaracteres/palindromeCadenas.cpp
+++ intermediate/cadenasdeCaracteres/palindromeCadenas.cpp
@@ -5,10 +5,14 @@
 using namespace std;
 
 int main(){
-	string word = "reconocer";
-	//string word = "eduardo";
+	//string word = "reconocer";
+	string word;
+	cout << "Escriba una palabra: ";
+	getline(cin, word);
 	string originalWord = word;
 	reverse(word.begin(), word.end());
+	cout << "Palabra: " << originalWord << endl;
+	cout << "Palabra invertida: " << word << endl;
 	(word == originalWord) ? cout << "La palabra es palindroma" : cout << "No es una palindroma";
 	cout << endl;
 	return 0;
