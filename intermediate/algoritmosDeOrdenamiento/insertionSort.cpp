e200fcf52eecd88bb81a8db915697634611f1787
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-16 21:05:06 -0500

    insertion sort compressed

--- intermediate/algoritmosDeOrdenamiento/insertionSort.cpp
+++ intermediate/algoritmosDeOrdenamiento/insertionSort.cpp
@@ -8,26 +8,38 @@ int main(){
 	//O(n^2)
 	// Es ineficiente para arreglos mayores a 50 datos
 
-	int numeros[] = {10,5,3,9,2,8,4,7,6,1};
+	int numeros[] = {10,5,3,9,2,8,4,7,6,1,19,13,14,12,17,18,16,15,11,20};
 	int length = (sizeof(numeros)/sizeof(*numeros));
-
-	//algoritmo
-	//for(int i = 0;i<length;i++) for(int j = 0;j < length;j++) (j != 0) ?
-	//								((numeros[j-1] > numeros[j]) ? swap())
-	//							: (void(0));
 
 	cout << "Length: " << length << endl;
 	cout << "El arreglo es: " << endl; for(int &numero : numeros) cout << numero << endl;
-	int aux, pos;
-	for(int i=0;i < length;i++){
-		aux = numeros[i];
-		pos = i;
-		while((pos > 0) && (numeros[pos-1] > aux)){
-			numeros[pos] = numeros[pos-1];
-			pos--;
-		}
-		numeros[pos] = aux;
-	}
+
+	// algoritmo de la clase
+
+	//int aux, pos;
+	//for(int i=0;i < length;i++){
+	//	aux = numeros[i];
+	//	pos = i;
+	//	while((pos > 0) && (numeros[pos-1] > aux)){
+	//		numeros[pos] = numeros[pos-1];
+	//		pos--;
+	//	}
+	//	numeros[pos] = aux;
+	//}
+
+	//algoritmo minimizado
+
+	//for(int i = 0;i < length;i++){
+	//	for(int j = i;j > 0;j--){
+	//		if(numeros[j-1] > numeros[j]){
+	//			swap(numeros[j-1],numeros[j]);
+	//		}
+	//	}
+	//}
+
+	// algoritmo ofuscado
+	for(int i = 0;i < length;i++) for(int j = i;j > 0;j--) (numeros[j-1] > numeros[j]) ? swap(numeros[j-1],numeros[j]) : (void(0));
+
 	// imprimiendo el arreglo ordenado
 	cout << "El arreglo ordenado es: " << endl; for(int &numero : numeros) cout << numero << endl;
 	return 0;
