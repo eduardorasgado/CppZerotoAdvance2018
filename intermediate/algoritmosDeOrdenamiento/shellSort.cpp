102eb90aefc335c86e5d832b4b4bb207e0e9c8ef
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-17 03:55:03 -0500

    shell sort developed and compressed

--- intermediate/algoritmosDeOrdenamiento/shellSort.cpp
+++ intermediate/algoritmosDeOrdenamiento/shellSort.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <algorithm>
-
+#include <cmath>
 using namespace std;
 
 int main(){
@@ -14,6 +14,25 @@ int main(){
 	// 	decreciente en la mitad que el salto anterior y luego clasificando cada grupo por separado
 	// El algoritmo termina cuando el tamaño del salto es 1
 
-	
+	int numeros[] = {20,15,13,9,12,8,4,7,6,1,19,3,14,2,17,18,16,5,11,10};
+
+	int length = sizeof(numeros)/sizeof(*numeros);
+
+	cout << "*******ALGORITMO DE ORDENAMIENTO: SHELL SORT*********" << endl;
+	cout << "El arreglo es el siguiente" << endl; for (int &numero : numeros) cout << numero << endl;
+	cout << "-------------------------------" << endl;
+
+	// algoritmo
+	// sorted: flag para saber si se ordenaron todos en le intervalo o no
+	int gap = length/2, sorted = 0;
+	while(gap >= 1){
+		// iterando en intervalos
+		for(int j = 0; j < (length-gap);j++) (numeros[j] > numeros[j+gap]) ? swap(numeros[j], numeros[j+gap]), sorted++ : 0;
+		// en caso de aun no estar ordenado cada grupo gap sigue siendo igual
+		gap = (sorted == 0) ? floor(gap/2) : gap, sorted = 0;
+	}
+
+	// imprimimos en orden asc, el vector resultante
+	cout << "El resultado es: " << endl; for (int &numero : numeros) cout << numero << endl;
 	return 0;
 }
