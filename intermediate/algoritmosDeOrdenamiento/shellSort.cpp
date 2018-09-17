653cbfd7d0eb0a79090ee21f8525e90646ee27da
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-17 02:45:58 -0500

    shell sort algorithm

---
+++ intermediate/algoritmosDeOrdenamiento/shellSort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	// Algoritmo de ordenamiento Shell Sort
+	// Pasos
+	//Se divide la lista en n/2, considerando un incremento entre los dos elementos
+	// Se clasifica cada grupo por seárado comparando las parejas de elementos y si no estan ordenados se intercambian
+	// Se divide ahora la lista en la mitad de grupos n/4 cpn un salto entre los elementos tambien de la mitad
+	//   y tambien se separa cada grupo por separado
+	// Asi sucesivamente se sigue dividiendo la lista en la mitad de grupos que en el recorrido anterior con un salto
+	// 	decreciente en la mitad que el salto anterior y luego clasificando cada grupo por separado
+	// El algoritmo termina cuando el tamaño del salto es 1
+
+	
+	return 0;
+}
