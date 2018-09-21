635a24aa875bd034154f9d6daa671a1365c9ff17
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-20 21:40:52 -0500

    quick sort p1

---
+++ intermediate/algoritmosDeOrdenamiento/quickSort.cpp
@@ -0,0 +1,20 @@
+#include <iostring>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	// Algoritmo de ordenamiento: Quick sort
+	// divide and conquer
+	// O(nlog(n))
+	// Pasos:
+	// -Selecciona el elemento central de a[] como pivote
+	// -Divide los elementos restantes en particiones izquierda y derecha, de modo que ningun elemento
+	// de la izquierda tenga una clave mayor que el pivote y que ningun elemento a la derecha tenga
+	// una clave mas pequenia que la del pivote
+	// -Ordenar la particion izquierda utilizando quick sort recursivamente
+	// Ordenar la particion derecha utilizando quick sort recursivamente
+	// La solucion es particion izquierda seguida por el pivote y la particion derecha
+	return 0;
+	
+}
