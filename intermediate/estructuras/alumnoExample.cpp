8c5f16ffac043165453a12de4216354d1e314439
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-21 22:48:46 -0500

    best note in student example

--- intermediate/estructuras/alumnoExample.cpp
+++ intermediate/estructuras/alumnoExample.cpp
@@ -14,22 +14,29 @@ alumnos[3]
 int main()
 {
 	// Ejemplo de alumnos: 3 alumnos e imprimir el mas alto de promedio
-	int major;
+	int major = 0;
 	for(int i = 0;i < 3;i++)
 	{
-		cout << "=====ALUMNO " << i << " =======" << endl;
+		cout << "=====ALUMNO " << i+1 << " =======" << endl;
 		cout << "Nombre: ", cin >> alumnos[i].nombre;
 		cout << "Edad: ", cin >> alumnos[i].edad;
 		cout << "Promedio: ", cin >> alumnos[i].promedio;
 	}
 
-	cout << "=================================" << endl;
+	cout << "===============ALUMNOS==================" << endl;
 	for(int i = 0;i < 3;i++)
         {
 		cout << "Nombre: " << alumnos[i].nombre << endl;
 		cout << "Edad: " << alumnos[i].edad << endl;
 		cout << "Promedio: " << alumnos[i].promedio << endl;
 		cout << endl;
+		major = (alumnos[i].promedio > alumnos[major].promedio) ? i : major;
 	}
+
+	cout << "=========MEJOR ALUMNO=============" << endl;
+	cout << "Nombre: " << alumnos[major].nombre << endl;
+        cout << "Edad: " << alumnos[major].edad << endl;
+        cout << "Promedio: " << alumnos[major].promedio << endl;
+        cout << endl;
 	return 0;
 }
