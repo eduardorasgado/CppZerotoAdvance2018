7153b0bc1e151817848c332104075372c062e463
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-22 15:03:17 -0500

    biker time calculation

---
+++ intermediate/estructuras/bikerExample.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+struct Etapa
+{
+	int horas;
+	int minutos;
+	int segundos;
+};
+
+int main()
+{
+	int N, secondsLeft, minutesLeft;
+	cout << "Cuantas etapas son: ", cin >> N;
+	struct Etapa etapas[N];
+
+	int totalTimeHours = 0;
+	int totalTimeMinutes = 0;
+	int totalTimeSeconds = 0;
+
+	for(int i = 0;i<N;i++)
+	{
+		cout << "===ETAPA " << i << "===" << endl;
+		cout << "Horas: ", cin >> etapas[i].horas;
+		cout << "Minutos: ", cin >> etapas[i].minutos;
+		cout << "Segundos: ", cin >> etapas[i].segundos;
+		cout << endl;
+
+		totalTimeHours += etapas[i].horas;
+		totalTimeMinutes += etapas[i].minutos;
+		totalTimeSeconds += etapas[i].segundos;
+	}
+	cout << "Tiempo total del corredor-> " << "Horas: " << totalTimeHours << ", Minutos: " << totalTimeMinutes << ", Segundos: " << totalTimeSeconds << endl;
+	// refactorizado del tiempo
+	totalTimeHours += (totalTimeMinutes / 60);
+	totalTimeMinutes = (totalTimeMinutes % 60) + (totalTimeSeconds / 60);
+	totalTimeSeconds = (totalTimeSeconds % 60);
+
+	// imprimiendo las horas totales:
+	cout << "Tiempo total del corredor: " << totalTimeHours << ":" << totalTimeMinutes << ":" << totalTimeSeconds << endl;
+	return 0;
+}
+
