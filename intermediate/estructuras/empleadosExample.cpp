024467b72bb35b3a52e7e56613ed0829a3815914
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-22 00:00:20 -0500

    empleados example

---
+++ intermediate/estructuras/empleadosExample.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+struct Empleado
+{
+	string nombre;
+	char genero[1];
+	float salario;
+};
+
+int main()
+{
+	// preguntar numero de empleados y calcular empleado con mayor y menor salario
+	int N;
+	cout << "Ingrese el numero de empleados que desea ingresar: ", cin >> N;
+	struct Empleado empleados[N];
+
+	// peticion de datos
+	
+	return 0;
+}
