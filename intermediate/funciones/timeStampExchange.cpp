a169dd22d87d80df1eb67d85cc4d20fe2bb5ebbf
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-22 21:54:12 -0500

    seconds formatting with references

---
+++ intermediate/funciones/timeStampExchange.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+// prototipos
+void tiempo(int totalSeg, int&, int&, int&);
+void showTime(int&, int&, int&);
+
+int main(int argc, char** argv)
+{
+	int totalSegundos, hours = 0, minutes = 0, seconds = 0;
+	cout << "Ingrese la cantidad de segundos a convertir: ", cin >> totalSegundos;
+
+	cout << "La cantidad de segundos es: " << totalSegundos << endl;
+
+	tiempo(totalSegundos, hours, minutes, seconds);
+	showTime(hours, minutes, seconds);
+
+	return 0;
+}
+
+void tiempo(int totalSeg, int& horas, int& minutos, int& segundos)
+{
+	// realiza la conversion del tiempo en segundos a un formato mas
+	// digerible
+	horas = totalSeg / 3600;
+	totalSeg %= 3600;
+	minutos = totalSeg / 60;
+	totalSeg %= 60;
+	segundos = totalSeg;
+}
+
+void showTime(int& horas, int& minutos, int& segundos)
+{
+	cout << "======TIEMPO====" << endl;
+	cout << horas << ":" << minutos << ":" << segundos << endl;
+}
+
