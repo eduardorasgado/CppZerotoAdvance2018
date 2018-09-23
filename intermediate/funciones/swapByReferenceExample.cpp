c4f5bf9e807a1b32b2ca99cfc70fd36c4f53af33
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-22 21:04:54 -0500

    swapping values with references finished

--- intermediate/funciones/swapByReferenceExample.cpp
+++ intermediate/funciones/swapByReferenceExample.cpp
@@ -2,8 +2,32 @@
 
 using namespace std;
 
+void swapper(int&, int&);
+void showValues(int&, int&);
+
 int main(int argc, char** argv)
 {
 	// intercambio de dos variables por referencia
+	int num1 = 10, num2 = 25;
+	showValues(num1, num2);
+
+	cout << "intercambio..." << '\n' << endl;
+	swapper(num1, num2);
+
+	showValues(num1, num2);
 	return 0;
+}
+
+void swapper(int& x, int& y)
+{
+	//swapping by reference
+	int aux = y;
+	y = x;
+	x = aux;
+}
+
+void showValues(int& num1, int& num2)
+{
+	cout << "El numero 1 es: " << num1 << endl;
+	cout << "El numero 2 es: " << num2 << endl;
 }
