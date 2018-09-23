a972cfa53dac520fd9d6640c6cff00dd10b1e594
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-22 19:55:03 -0500

    unique argument to template function

--- intermediate/funciones/landExample.cpp
+++ intermediate/funciones/landExample.cpp
@@ -7,9 +7,15 @@ void despliegue(TYPED argumento);
 
 int main(int argc, char** argv)
 {
-	double arg = 124.4564;
+	int arg0 = 15;
+	double arg1 = 1124.4564;
+	float arg2 = 15.56;
+	char arg3 = 'b';
 
-	despliegue(arg);
+	despliegue(arg0);
+	despliegue(arg1);
+	despliegue(arg2);
+	despliegue(arg3);
 
 	return 0;
 }
