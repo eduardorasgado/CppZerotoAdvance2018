ac55c62958a5af40d2eec78a6ff6b554cd032810
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-09-26 14:39:25 -0500

    Even and odds using pointers: finished

--- advanced/punteros/evenOdds.cpp
+++ advanced/punteros/evenOdds.cpp
@@ -2,36 +2,36 @@
 
 using namespace std;
 
-void dataRequest(int*);
+void dataRequest(int&);
 void computeOdds(int*);
 
 int main(void)
 {
     // numero / puntero para guardar
     int n, *dir_n;
-    // guardando la ubucacion doe memoria de n
+    // guardando la ubucacion de memoria de n
     dir_n = &n;
     // mandando la direccion de memoria
-    dataRequest(dir_n);
+    dataRequest(n);
 
     computeOdds(dir_n);
 
     return 0;
 }
 
-void dataRequest(int *dir_n)
+void dataRequest(int& n)
 // llamando por puntero
 {
     //guardando el dato en la variable que guarda el espacio de memoria
-    cout << "Inserte el numero a guardar: ", cin >> *dir_n;
+    cout << "Inserte el numero a guardar: ", cin >> n;
     cout << endl;
 }
 
 void computeOdds(int *dir_n)
 {
     // coprobando si un valor es par o impar
-    if(*dir_n % 2 == 0) cout << "El numero es par" << endl;
-    else cout << "El numero es impar" << endl;
+    if(*dir_n % 2 == 0) cout << "El numero " << *dir_n << " es par" << endl;
+    else cout << "El numero " << *dir_n << " es impar" << endl;
 
     // imprimir la ubicacion e memoria del puntero
     cout << "La ubicacion de memoria es: " << dir_n << endl;
