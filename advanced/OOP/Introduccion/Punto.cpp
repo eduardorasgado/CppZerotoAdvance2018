6a31e2af57f95a7626a1e5c5e9e28d88281ddbaa
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-01 02:27:45 -0500

    Header and cpp for a class, methods and attributes

---
+++ advanced/OOP/Introduccion/Punto.cpp
@@ -0,0 +1,23 @@
+//
+// Created by cheetos on 1/10/18.
+//
+#include "Punto.h"
+
+// Establecemos el valor de x
+void Punto::setX(int valorX)
+{
+    x = valorX;
+}
+void Punto::setY(int valorY)
+{
+    y = valorY;
+}
+
+int Punto::getX()
+{
+    return x;
+}
+int Punto::getY()
+{
+    return y;
+}
--- advanced/OOP/Introduccion/Punto.h
+++ advanced/OOP/Introduccion/Punto.h
@@ -2,6 +2,33 @@
 
 class Punto
 {
-    // ATRIBUTOS DE LA CLASE
+    private:
+        // ATRIBUTOS DE LA CLASE
+        // privada: solo pueden acceder los metodos de esta clase
+        int x, y;
+
+    public:
+        //METODOS DE LA CLASE
+        //publica: pueden acceder toodo aquello ajeno a la clase
+        //CONSTRUCTOR DE LA CLASE
+        Punto(int _x, int _y) // CONSTRUCTOR 1
+        {
+            x = _x;
+            y = _y;
+        }
+
+        Punto() // CONSTRUCTOR 2: VALORES DEFAULT
+        {
+            x = y= 0;
+        }
+
+        // PROTOTIPOS DE METODOS
+
+        // METODOS SETTERS
+        void setX(int valorX);
+        void setY(int valorY);
+        // METODOS GETTERS
+        int getX();
+        int getY();
 
 };
