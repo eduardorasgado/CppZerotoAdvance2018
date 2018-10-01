6cd95f04a62baa12a419c12d2fb42e8b822e210a
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-01 03:09:03 -0500

    Creating an instance: static and dynamic modes.

--- advanced/OOP/Introduccion/main.cpp
+++ advanced/OOP/Introduccion/main.cpp
@@ -10,5 +10,17 @@ int main() {
     // accediendo a los metodos de la clase creada
     cout << "El valor de x es: "<< point1.getX() << endl;
     cout << "El valor de y es: " << point1.getY() << endl;
+
+    cout << "=========" << endl;
+
+    // no seteamos nada porque usamos el constructor 2: default
+    Punto* point2 = new Punto(); // OBJETO DINAMICO
+
+    //para objetos dinamicos utilizamosla flechita
+    point2->setX(4);
+    point2->setY(16);
+
+    cout << "El valor de x es: "<< point2->getX() << endl;
+    cout << "El valor de y es: " << point2->getY() << endl;
     return 0;
 }
