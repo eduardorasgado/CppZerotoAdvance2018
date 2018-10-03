4deaa983d9b380089594942f2e37f41bfb95b523
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-03 14:28:35 -0500

    Object arrays. p2: functions for objects array handling.

--- advanced/OOP/objectsArray/main.cpp
+++ advanced/OOP/objectsArray/main.cpp
@@ -3,15 +3,36 @@
 
 using namespace std;
 
+int requestN();
+void requestingData(Alumno *, int&);
+
 int main() {
     // CREANDO ARREGLO DE OBJETOS
 
+    int N = requestN();
+
     // ARREGLO ESTATICO DE OBJETOS
-    Alumno alumnos[4];
+    Alumno alumnos[N];
 
     // ARREGLO DINAMICO DE OBJETOS
-    Alumno* alumnos2 = new Alumno[4];
+    Alumno* alumnos2 = new Alumno[N];
 
-
+    requestingData(alumnos2, N);
     return 0;
+}
+
+int requestN()
+{
+    int N;
+    cout << "Ingrese la cantidad de elemento de sus objetos: ";
+    cin >> N;
+    return N;
+}
+
+void requestingData(Alumno* alumnos2, int& N)
+{
+    for (int i = 0; i < N; ++i) {
+        (alumnos2 + i)->requestData();
+        cout << endl;
+    }
 }
