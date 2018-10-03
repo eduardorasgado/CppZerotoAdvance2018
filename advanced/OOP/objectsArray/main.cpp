09c0286173c00cc1ac655df00331c05e30714955
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-03 14:32:28 -0500

    Object arrays. p2: functions to show Data

--- advanced/OOP/objectsArray/main.cpp
+++ advanced/OOP/objectsArray/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int requestN();
 void requestingData(Alumno *, int&);
+void showingData(Alumno *, int&);
 
 int main() {
     // CREANDO ARREGLO DE OBJETOS
@@ -18,6 +19,9 @@ int main() {
     Alumno* alumnos2 = new Alumno[N];
 
     requestingData(alumnos2, N);
+
+    showingData(alumnos2, N);
+
     return 0;
 }
 
@@ -33,6 +37,16 @@ void requestingData(Alumno* alumnos2, int& N)
 {
     for (int i = 0; i < N; ++i) {
         (alumnos2 + i)->requestData();
+        cout << endl;
+    }
+}
+
+void showingData(Alumno* alumnos2, int& N)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        cout << "====ALUMNO " << i+1 << "=====" << endl;
+        (alumnos2+i)->showNotes();
         cout << endl;
     }
 }
