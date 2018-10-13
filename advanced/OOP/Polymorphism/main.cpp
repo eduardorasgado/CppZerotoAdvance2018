a23e2d147e91b6760ea1450acd9da9bcaf4d7d52
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-12 23:25:18 -0500

    Polimorphism: finished.

--- advanced/OOP/Polymorphism/main.cpp
+++ advanced/OOP/Polymorphism/main.cpp
@@ -3,6 +3,8 @@
 #include "Rectangulo.h"
 #include "Triangulo.h"
 
+using namespace Figure;
+
 int main() {
     /*
      * POLIMORFOSMO:
@@ -19,7 +21,20 @@ int main() {
      * */
 
     // creando dos objetos
-    Figure::Poligono* poligonos[2];
+    // Figure::Poligono* poligonos[2]
+    Poligono* poligonos[2];
+
+    // instanciando los objetos creados
+    poligonos[0] = new Rectangulo(7, 4);
+    poligonos[1] = new Triangulo(3, 4, 4);
+
+    for(int i = 0; i < 2; i++)
+    {
+        // Mostrando el polimorfismo
+        std::cout << "===Figura " << i+1 << std::endl;
+        std::cout << "Area: " << poligonos[i]->area() << std::endl;
+        std::cout << "Perimentro: " << poligonos[i]->perimetro() << std::endl;
+    }
 
     return 0;
 }
