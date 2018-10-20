b373f3ed7bdd7240f5daf2a09c24d98afded3e8e
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-19 21:26:40 -0500

    Random Access Iterators: rbegin and rend, begin()+n and end()-n

--- advanced/STL/RandomAccessIterator/main.cpp
+++ advanced/STL/RandomAccessIterator/main.cpp
@@ -7,7 +7,7 @@
 #include "VectorMethods.h"
 
 int main() {
-    // Usando iteradores bidireccionales
+    // Usando iteradores random
     // creando un vector dinamicamente almacenado
     std::vector<float> *myVector = new std::vector<float>;
     //agregndo elementos al vector
@@ -21,10 +21,19 @@ int main() {
     std::cout << "Forward showing..." << std::endl;
     showingVector(i, f, true);
 
-    i = myVector->end()-1;
-    f = myVector->begin()-1;
+    //i = myVector->end()-1;
+    //f = myVector->begin()-1;
+    auto ii = myVector->rbegin(); // Apuntar a lo contrario del inicio
+    auto ff = myVector->rend(); // lo mismo de arriba pero con el final
     std::cout << "Backward showing..." << std::endl;
-    showingVector(i, f, false);
+    // showingVector(i, f, false);
+    showingVector(ii, ff, true);
+
+    std::cout << "Showing elements in the middle..." << std::endl;
+    //Visualizar los elementos del medio del vector: (1-2) indices
+    showingVector(myVector->begin()+1, myVector->begin()+3,true);
+    // tambien podemos hacerlo de la siguiente forma:
+    // myVector->end()-1
 
     delete myVector;
 
