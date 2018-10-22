9cbf4ae7004216083c97c33e75147ea235bcc905
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-22 09:13:54 -0500

    SETS. p2: Operations over a set.

--- advanced/STL/setContainer/main.cpp
+++ advanced/STL/setContainer/main.cpp
@@ -13,6 +13,12 @@ void addRandomValues(std::set<T>*);
 template <typename T>
 void showTheSet(std::set<T>*);
 
+template <typename T>
+void searchInSet(std::set<T>*);
+
+template <typename T>
+void poppingFromSeT(std::set<T>*);
+
 int main() {
     // SETS
     /*
@@ -27,6 +33,19 @@ int main() {
     // operaciones con el set
     addRandomValues(values);
     showTheSet(values);
+    // volviendo a agregar para ver como ignora los duplicados
+    //addRandomValues(values);
+    //showTheSet(values);
+
+    //busqueda de un elemento dentro del set
+    searchInSet(values);
+    // eliminando un elemento dentro del set
+    poppingFromSeT(values);
+    showTheSet(values);
+
+    // borrando el set creado dinamicamente
+    delete values;
+
     return 0;
 }
 
@@ -55,4 +74,44 @@ void showTheSet(std::set<T>* values)
     std::ostream_iterator<T> salida(std::cout, " ");
     // usamos el iterador, copiando los elementos a la salida
     std::copy(values->begin(), values->end(), salida);
+
+    std::cout << std::endl;
+}
+
+template <typename T>
+void searchInSet(std::set<T>* values)
+{
+    // se le pide al usuario el elemento que se va a busca
+    T N;
+    std::cout << "Buscar el elemento: ", std::cin >> N;
+
+    std::set<int>::iterator i;
+    // buscando l elemento y guardandolo dentro de un iterador
+    i = values->find(N);
+
+    // imprimiendo el resultado
+    if(i != values->end()) std::cout << "El elemento " << *i << " fue encontrado." << std::endl;
+    else std::cout << "No se encontró el elemento." << std::endl;
+}
+
+template <typename T>
+void poppingFromSeT(std::set<T>* values)
+{
+    std::cout << "Eliminando elementos del set" << std::endl;
+    showTheSet(values);
+    T N;
+
+    for(;;)
+    {
+        std::cout << "Número a eliminar: ", std::cin >> N;
+        // buscando el elemento agreagado para poder eliminarlo
+        std::set<int>::iterator i = values->find(N);
+        if (i != values->end()) break;
+        else std::cout << "El elemento " << N << " no esxiste en el set." << std::endl;
+    }
+
+    std::cout << "Eliminando elemento..." << std::endl;
+    // eliminando un elemento dentro del set, previamente comprobado su existencia(del elemento)
+    values->erase(N);
+    std::cout << std::endl;
 }
