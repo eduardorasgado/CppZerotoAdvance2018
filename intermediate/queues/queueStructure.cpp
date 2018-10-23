c8af4bf0a29c8a3705cdedc2fca4d232c566988b
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-22 23:03:41 -0500

    Queues: getting started

---
+++ intermediate/queues/queueStructure.cpp
@@ -0,0 +1,14 @@
+/*
+*COLAS
+* FIFO: First Input First Output
+* Este tipo de estructuras se caracteriza por que
+* en ella l aoperacion de insercion se realiza por
+* un extremo y la extraccion por el otro lado
+**/
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+	return 0;
+}
