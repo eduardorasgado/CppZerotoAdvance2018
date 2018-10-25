8de60e0b01630381d815a7cfca4eb4db72a2cc95
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-25 10:13:51 -0500

    understanding passing by value, pointer and reference param, using const

--- modernCpp/deducingTypes/practice1.cpp
+++ modernCpp/deducingTypes/practice1.cpp
@@ -11,7 +11,14 @@ template <typename T>
 T equationOne(const T&);
 
 template <typename T>
-void manipulateNewValue(T& value);
+void manipulateNewValue(T&& value);
+
+template <typename T>
+void handleFunc2(T value);
+
+void changingArray(int[]);
+
+void changingArrayPointer(int*);
 
 int main()
 {
@@ -21,6 +28,26 @@ int main()
 	// passing the value as a reference
 	auto new_value = equationOne(value);
 	manipulateNewValue(new_value);
+
+	// ptr is const pointer to const
+	const char* const ptr = "Fun with pointers";
+	// pass arg of type char * const
+	// the pointer itself is passed by value
+	// because this functiono receives values
+	// pssed by values
+	handleFunc2(ptr);
+
+	// Array arguments
+	const char cypher[] = "Red$ew75vPBqAV4z";
+	// constant pointer to array of characters
+	const char * ptrToCypher = cypher;
+	handleFunc2(ptrToCypher);
+
+	int myArrayInt[] = {1,2,3,4,5,6,7,8,9};
+	//passing by value
+	changingArray(myArrayInt);
+	// psasing by pointer
+	changingArrayPointer(myArrayInt);
 	return 0;
 }
 
@@ -33,9 +60,39 @@ T equationOne(const T& value)
 	return number;
 }
 
+// here we see a universal reference
 template <typename T>
-void manipulateNewValue(T& value)
+void manipulateNewValue(T&& value)
 {
 	// do somthing with the received value
 	std::cout << "The value is: " << value << std::endl;
+}
+
+// passing by value
+template <typename T>
+void handleFunc2(T value)
+{
+	std::cout << value << std::endl;
+	// lets try to changing the value to show it cant
+	// be changing instead
+	//value = (value*value) / (value*value*value)
+	// it will show an error instead
+}
+
+void changingArray(int myArray[])
+{
+	// do something
+	int length = sizeof(&myArray);
+	std::cout << "Length is this: " << length << std::endl;
+	for(int i = 0;i <length;++i)
+		std::cout << myArray[i] << " ";
+	std::cout << std::endl;
+}
+// function above is the same as function here bellow
+void changingArrayPointer(int *myArray)
+{
+	// do something
+	int length = sizeof(&myArray);
+	for(int i = 0;i <length;++i) std::cout << myArray[i] << " ";
+	std::cout << std::endl;
 }
