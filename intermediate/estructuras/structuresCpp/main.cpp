db3e948f34f64953da48e1f86f442cd09831b7d1
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-27 23:58:18 -0500

    structures and classes C11: make_shared and initializer_list in a method

--- intermediate/estructuras/structuresCpp/main.cpp
+++ intermediate/estructuras/structuresCpp/main.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
+#include <memory>
+#include <initializer_list>
 
+template <class T>
 class Holiday
 {
     private:
         std::string name;
-        const int day;
-        const int month;
+        int day;
+        int month;
     public:
-        Holiday(std::string name, const int day, const int month)
+        Holiday(std::string name, int day, int month)
         : name{name}, day{day}, month{month}{}
         Holiday() = default; // force the compiler
+
+        // setting the names pased in a vector form
+        void setNums(const std::initializer_list<T>& vect)
+        {
+            auto i = vect.begin();
+            this->day = *i;
+            i++;
+            this->month = *i;
+        }
+        void setName(std::string n) { this->name = n; }
 };
 // User defined types
 struct Date
@@ -24,6 +38,9 @@ struct Date
 
 void insertingValues(Date*);
 
+template <typename T>
+void doingSomething(std::vector<T>*, std::string);
+
 int main() {
     // structures
     struct Date d1{12, 9,2018};
@@ -36,6 +53,13 @@ int main() {
     Date *d2 = new Date();
     insertingValues(d2);
     std::cout << "Dia: " << d2->day << std::endl;
+
+    // using class Holiday safely inside a function
+    std::vector<int> v = {d2->day, d2->month};
+    std::vector<int> *pv = &v;
+    doingSomething(pv, "Christmas");
+
+    delete d2;
     return 0;
 }
 
@@ -45,4 +69,16 @@ void insertingValues(Date* d)
     std::cout << "Inserte dia: ", std::cin >> d->day;
     std::cout << "Inserte mes: ", std::cin >> d->month;
     std::cout << "Inserte año: ", std::cin >> d->year;
+}
+
+template <typename T>
+void doingSomething(std::vector<T>* v, std::string)
+{
+    // using a shared pointer
+    // we are using the default constructor of holiday
+    auto christmas = std::make_shared<Holiday<T>>();
+    // so when this function finishes, we do not have to delete a pointer
+    // because we are using a smart pointer
+    christmas->setNums({v->at(0), v->at(1)});
+
 }
