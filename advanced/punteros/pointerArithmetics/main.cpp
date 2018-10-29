626e00048a6c892a2fcbc68b5ec0019aabe6f69d
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-28 22:45:57 -0600

    pointer arithmetic p2.

--- advanced/punteros/pointerArithmetics/main.cpp
+++ advanced/punteros/pointerArithmetics/main.cpp
@@ -19,8 +19,44 @@ int main() {
     //create a pointer to that string
     auto *pTexts = texts;
     showing(pTexts);
+    // addition
     pTexts = pTexts+1;
     showing(pTexts);
+    pTexts += 3;
+    showing(pTexts);
+    // substraction
+    pTexts -= 2;
+    showing(pTexts);
+
+    std::cout << "Last element in array: ";
+    auto *pEnd = &texts[sizeArray(texts)-1];
+    showing(pEnd);
+
+    // creating another pointer to last element of the array
+    auto *pEnd2 = texts+(sizeArray(texts)-1);
+    showing(pEnd2);
+    if(pEnd==pEnd2) std::cout << "pEnd and pEnd2 are pointing same element in memory" << std::endl;
+
+    // looping over an array by comparing two pointers
+    pTexts =  texts;
+    while(pTexts != pEnd2+1)
+    {
+        showing(pTexts);
+        pTexts++;
+    }
+    std::cout << "last element: " << texts[NSTRINGS-1] << std::endl;
+
+    // returning pointers to their initial values
+    pTexts = &texts[0];
+    // pointing to address to last element of the array + the next location in memory
+    pEnd = &texts[NSTRINGS-1]+1;
+    // casting  to long: an array usually has a lot of values
+    long elements = (long)(pEnd - pTexts);
+    std::cout << "Elements in the array: " << elements << std::endl;
+
+    // the middle of the array
+    pTexts += NSTRINGS/2;
+    std::cout << "Middle of the array: "; showing(pTexts);
 
     return 0;
 }
