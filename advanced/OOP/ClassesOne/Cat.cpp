7dbb3e55d3109b528659f7db2b7bdab124d2b6df
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-28 18:07:23 -0600

    Classes bases

---
+++ advanced/OOP/ClassesOne/Cat.cpp
@@ -0,0 +1,15 @@
+//
+// Created by cheetos on 28/10/18.
+//
+
+#include "Cat.h"
+#include <iostream>
+
+void Cat::speak()
+{
+    std::cout << "meow" << std::endl;
+}
+void Cat::jump()
+{
+    std::cout << "Jumping from a building" << std::endl;
+}
---
+++ advanced/OOP/ClassesOne/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Cat.h"
+
+int main() {
+    // Classes
+    Cat c1;
+
+    c1.speak();
+    c1.jump();
+
+    return 0;
+}
