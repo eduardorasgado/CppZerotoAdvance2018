08273d2ab9f6016d48180f002ce2029776d7862c
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-29 19:36:40 -0600

    new keyword and delete: objects with dynamic allocation

--- intermediate/memoryAllocation/newKeyword.cpp
+++ intermediate/memoryAllocation/newKeyword.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <cstdlib>
 
 class Animal
 {
@@ -15,7 +16,7 @@ class Animal
         {
             std::cout << "Animal created by copying" << std::endl;
         }
-        ~Animal() { std::cout << "Object Destructed" << std::endl; } // Destructor
+        ~Animal() { std::cout << "Object Destructed: " << this->name << std::endl; } // Destructor
 
         void setName(std::string name) { this->name = name; }
         void speak() const
@@ -34,8 +35,18 @@ int main()
     cat1.setName("Botones");
     cat1.speak();
 
+    std::cout << "-------------------------" << std::endl;
     //create an object dinalically
+    Animal *pCat = new Animal();
+    // the way to use a dynamically allocated instance method
+    (*pCat).setName("Benito");
+    // but there is a better an even more elegant way to do this
+    pCat->speak();
 
+    // a pointer has same size as a long type: 8 bytes
+    std::cout << "Size of pointer: " << sizeof(pCat) << std::endl;
+
+    std::cout << "-------------------------" << std::endl;
 
     // create an object with a smart point
     std::string catName;
@@ -44,6 +55,9 @@ int main()
     //copying an object inside a function
     objectCopy(cat1);
 
+    // deallocating memory explicitly
+    delete pCat; // lets delete Benito from memory
+
     return 0;
 }
 
@@ -51,7 +65,7 @@ void createAndDestruct(std::string&)
 {
     //creating an object using a smart point
     auto kitty = std::make_shared<Animal>();
-
+    kitty->setName("Agretusuki");
     std::cout << "smart pointer destroys the object safely." << std::endl;
 }
 
@@ -60,6 +74,12 @@ void objectCopy(Animal& otherCat)
 {
     //
     std::cout << "Creating colitas..." << std::endl;
-    Animal colitas(otherCat);
+    //Animal colitas(otherCat);
+    auto colitas = std::make_shared<Animal>(otherCat);
+    std::cout << "Colitas was created through smart pointer" << std::endl;
+
+    colitas->speak();
+    colitas->setName("Colitas");
+    colitas->speak();
 
 }
