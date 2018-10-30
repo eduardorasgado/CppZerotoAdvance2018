4457127d658acfb62c6ad63580469dd408116749
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-29 22:18:37 -0600

    Memory allocation p2: array of new creation based objects

--- intermediate/memoryAllocation/memoryAllocation.cpp
+++ intermediate/memoryAllocation/memoryAllocation.cpp
@@ -20,15 +20,14 @@ class Animal
         void setName(std::string name) { this->name = name; }
 
         // methods
-        void speak()
+        void speak() const
         { (this->name != " ") ?
             std::cout << "Hi my name is " << this->name << std::endl
             : std::cout << "No name is setted yet" << std::endl; }
 };
 
 
-int main()
-{
+int main() {
     // creating an animal in the heap
     auto *bigotes = new Animal();
 
@@ -36,8 +35,30 @@ int main()
     bigotes->setName("Bigotes");
     bigotes->speak();
 
-    //-----------------------------------
-
     delete bigotes;
+    //-----------------------------------
+    // we can use new even with primitive types
+    int *pInt = new int;
+    delete pInt;
+
+    // Creating an array of objects with new
+    Animal *pAnimals = new Animal[10];
+
+    // iterating over the Animal array dynamically created
+    // and calling their methods
+    char l = 'a';
+    for (int i = 0; i < 10; ++i) {
+        // assigning names: a, b, c, d ...
+        (pAnimals+i)->setName(std::string(&l));
+        l++;
+    }
+    for (int j = 0; j < 10; ++j) {
+        (pAnimals+j)->speak();
+    }
+
+    // to deallocate an array of dynamically created objects
+    // created using new, we should use brackets
+    delete []pAnimals;
+
     return 0;
 }
