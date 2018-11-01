d1d4c189aa287aff33460ebd3708201399082b29
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-31 23:08:01 -0600

    pointer and references in functions handle arrays: dynamic arrays and templates

--- intermediate/memoryAllocation/arrayFunctions.cpp
+++ intermediate/memoryAllocation/arrayFunctions.cpp
@@ -3,13 +3,16 @@
  * */
 #include <iostream>
 
+// passing arrays not in a good manner
 void showArray1(const int, std::string texts[]);
-void showArray2(int&, std::string*);
+// passing arrays by pointers
+void showArray2(const int&, std::string*);
 
 // () parenthesis need to be there
 // this is recomended when want to use sizeof inside a function
-template <std::size_t N>
-void showArray3(std::string (&texts)[N]);
+// passing arrays by reference
+template <typename T, std::size_t N>
+void showArray3(T (&texts)[N]);
 
 // returning length of an array
 template <typename T, std::size_t N>
@@ -18,9 +21,26 @@ constexpr std::size_t returnLength(T(&)[N]) noexcept
     return N;
 }
 
+// return an array using pointer
+char *getArray(const int&);
+
+template <typename T>
+void handlingDynamicArray(const int&, T*);
+
+// setting free a char created with new
+// usually it s a common function seen in software
+template <typename T>
+void freeMemory(T *pMem)
+{
+    // deallocating the array
+    delete[] pMem;
+}
+
+// Could declare global arrays  here but not recomended
+
 int main()
 {
-    std::string text[] = {"text1", "text2", "text3", "text3", "hola"};
+    std::string text[] = {"text1", "text2", "text3", "text4", "hola"};
 
     //std::cout << sizeof(text)/ sizeof(std::string) << std::endl;
     int N = sizeof(text)/ sizeof(*text);
@@ -31,9 +51,18 @@ int main()
     std::cout << "------------" << std::endl;
     showArray3(text);
     std::cout << "------------" << std::endl;
-    // getting length of a string
-    auto nElements = returnLength(text);
-    std::cout << nElements << std::endl;
+    // getting length of first array and creating another
+    int nArray = returnLength(text);
+    std::cout << "L: " << nArray << std::endl;
+    // even when new is not used here we should deallocate the memory
+    auto *myArray = getArray(nArray);
+
+    // inserting values and showing those values in dynamic array
+    handlingDynamicArray(nArray, myArray);
+
+    std::cout << "\n------------" << std::endl;
+
+    freeMemory(myArray);
 }
 
 // pasing an array by value
@@ -51,7 +80,7 @@ void showArray1(const int elements, std::string texts[])
 }
 
 // passing as a pointer
-void showArray2(int& N, std::string *texts)
+void showArray2(const int& N, std::string *texts)
 {
     // int N = sizeof(*texts);
     //std::cout << N << std::endl;
@@ -64,13 +93,39 @@ void showArray2(int& N, std::string *texts)
 
 // passing by reference
 // and getting the size of the array using a size_t
-template <std::size_t N>
-void showArray3(std::string (&texts)[N])
+template <typename T, std::size_t N>
+void showArray3(T (&texts)[N])
 {
     std::cout << "show 3" << std::endl;
     // N can be used inside of the function
     for (int i = 0; i < N; ++i) {
         // even (&texts)[N] can be trated like this
         std::cout << *(texts+i) << std::endl;
+    }
+}
+
+// return array using a pointer
+// in a proper way
+char *getArray(const int& N)
+{
+    // Dont return pointers to local variables
+    //std::string texts[] = {"one", "two", "three", "four"};
+    //return texts;
+
+    // a proper way to return an array from a function
+    char *array = new char[N];
+    return array;
+}
+
+template <typename T>
+void handlingDynamicArray(const int& nArray, T*myArray)
+{
+    // inserting values into the array
+    for (int i = 0; i < nArray; ++i) {
+        *(myArray+i) = 'a'+i;
+    }
+    //showing the array
+    for (int j = 0; j < nArray; ++j) {
+        std::cout << *(myArray+j) << " ";
     }
 }
