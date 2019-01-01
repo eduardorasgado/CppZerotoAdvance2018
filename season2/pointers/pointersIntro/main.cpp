370c86d75487228a7574428b73c53a5b1e2bcc37
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-12-31 18:26:04 -0600

    pointers introduction p1

--- season2/pointers/pointersIntro/main.cpp
+++ season2/pointers/pointersIntro/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <iterator> // for ostream iterator
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    auto endLine = "\n";
+    auto metrics = new std::vector<std::string>();
+    metrics->push_back("Hello ");
+    metrics->push_back("STL World");
+
+    [&metrics](std::string line){
+        metrics->push_back(line);
+        std::ostream_iterator<std::string> output(std::cout, "");
+
+        std::copy(metrics->begin(), metrics->end(), output);
+    }(endLine);
+
+
     return 0;
 }
