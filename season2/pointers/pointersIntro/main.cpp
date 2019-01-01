1f8dc0cac4c96aa720b5817315023346ccc129bf
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-12-31 19:52:34 -0600

    intro pointers p2

--- season2/pointers/pointersIntro/main.cpp
+++ season2/pointers/pointersIntro/main.cpp
@@ -20,6 +20,20 @@ int main()
         std::copy(metrics->begin(), metrics->end(), output);
     }(endLine);
 
+    /*
+    * POINTERS
+    * &n = n address
+    * *n = n address storage
+    */
+    int num = 20;
+    std::cout << "Numer: " << num << std::endl;
+    std::cout << "num address: " << &num << std::endl;
 
+    // pointer: variable that store the memory address of another var
+    int *pNum = &num;
+
+    // dereference operator
+    std::cout << "pointer has the value: "<< *pNum << std::endl;
+    std::cout << "pointer mem address: " << pNum << std::endl;
     return 0;
 }
