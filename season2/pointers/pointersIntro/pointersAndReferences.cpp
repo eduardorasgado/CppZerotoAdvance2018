a95cb540bcba37b63368c030da1ec1d9c57ad112
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-04-12 19:42:27 -0500

    Working with pointers and references in different scopes p2

--- season2/pointers/pointersIntro/pointersAndReferences.cpp
+++ season2/pointers/pointersIntro/pointersAndReferences.cpp
@@ -27,26 +27,35 @@ class Printer {
 };
 
 void printStatement(Printer&);
+void printFromPointerObject(Printer*);
 
 int main() {
     std::string query = "Select * from users where used.id == $id";
-    Printer p1 = Printer();
+    auto p1 = Printer();
     p1.setQuery(query);
 
     Printer& p_ref = p1;
     p_ref.setQuery("DELETE * FROM users WHERE user.Id = $id");
 
     Printer* p_pointer = new Printer();
+    p_pointer->setQuery(query);
 
+    std::cout << "---before executing function---" << std::endl;
     printStatement(p_ref);
-    //printStatement(p1);
     std::cout << "after executing above function scope" << std::endl;
 
+    printFromPointerObject(p_pointer);
+    std::cout << "after calling function for pointers" << std::endl;
+
+    // p1 is deleted automatically since it is not dynamic
     //p_pointer = NULL;
     delete p_pointer;
     return 0;
 }
 
+/*
+* Reference as parameter, to avoid creation of a new Printer object for parameter object
+*/
 void printStatement(Printer& p) {
     // creating a dynamic object, stored in heap
     Printer* pScoped = new Printer();
@@ -61,4 +70,11 @@ void printStatement(Printer& p) {
 
     std::cout << "Doing another stuff inside printing function" << std::endl;
 
+}
+
+/*
+* Pointer as parameter, to avoid creation of a new Printer object, dynamic object handling
+*/
+void printFromPointerObject(Printer* p_pointer) {
+    std::cout << p_pointer->getQuery() << std::endl;
 }
